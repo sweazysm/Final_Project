0ef2b529fd5875322d8ad5506ce0ab62495b1ee5
Amilcar Vargas <85706721+a2zvargas@users.noreply.github.com>
2021-11-27 18:45:25 -0500

    updated to reflect new data types used

--- Code/CreateSchema.sql
+++ Code/CreateSchema.sql
@@ -1,27 +1,27 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
 CREATE TABLE "SevereWeatherEvents" (
     "event_id" INT   NOT NULL,
-    "state" VARCHAR(20)   NOT NULL,
-    "state_fips" INT   NOT NULL,
-    "year" INT   NOT NULL,
-    "month_name" VARCHAR(15)   NOT NULL,
-    "event_type" VARCHAR(50)   NOT NULL,
-    "injuries_direct" INT   NOT NULL,
-    "injuries_indirect" INT   NOT NULL,
-    "deaths_direct" INT   NOT NULL,
-    "deaths_indirect" INT   NOT NULL,
-    "damage_property" VARCHAR(15)   NOT NULL,
-    "damage_crops" VARCHAR(15)   NOT NULL,
-    "magnitude" INT   NOT NULL,
-    "category" VARCHAR(25)   NOT NULL,
-    "tor_f_scale" VARCHAR(5)   NOT NULL,
-    "tor_length" FLOAT   NOT NULL,
-    "tor_width" INT   NOT NULL,
-    "begin_lat" FLOAT   NOT NULL,
-    "begin_lon" FLOAT   NOT NULL,
+    "state" VARCHAR(20),
+    "state_fips" INT,
+    "year" INT,
+    "month_name" VARCHAR(15),
+    "event_type" VARCHAR(50),
+    "injuries_direct" INT,
+    "injuries_indirect" INT,
+    "deaths_direct" INT,
+    "deaths_indirect" INT,
+    "damage_property" VARCHAR(25),
+    "damage_crops" VARCHAR(25),
+    "magnitude" FLOAT,
+    "category" FLOAT,
+    "tor_f_scale" VARCHAR(5),
+    "tor_length" FLOAT,
+    "tor_width" FLOAT,
+    "begin_lat" FLOAT,
+    "begin_lon" FLOAT,
     CONSTRAINT "pk_SevereWeatherEvents" PRIMARY KEY (
         "event_id"
      )
@@ -44,8 +44,8 @@ CREATE TABLE "WorldCO2byYear" (
     "methane_per_capita" FLOAT   NOT NULL,
     "nitrous_oxide" FLOAT   NOT NULL,
     "nitrous_oxide_per_capita" FLOAT   NOT NULL,
-    "population" BIGINT   NOT NULL,
-    "gdp" BIGINT   NOT NULL,
+    "population" FLOAT   NOT NULL,
+    "gdp" FLOAT   NOT NULL,
     "primary_energy_consumption" FLOAT   NOT NULL,
     "energy_per_capita" FLOAT   NOT NULL,
     "energy_per_gdp" FLOAT   NOT NULL,
@@ -56,4 +56,3 @@ CREATE TABLE "WorldCO2byYear" (
 
 ALTER TABLE "SevereWeatherEvents" ADD CONSTRAINT "fk_SevereWeatherEvents_year" FOREIGN KEY("year")
 REFERENCES "WorldCO2byYear" ("year");
-
